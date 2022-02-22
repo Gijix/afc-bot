2d1b437ef242201027f2e3f8fab8b07b2bb9322c
Pedram Aguiard <aguiardpedram@gmail.com>
2022-02-22 01:02:16 +0100

    reply meme gif

--- index.js
+++ index.js
@@ -62,6 +62,33 @@ bot.on('messageCreate', (message) => {
     }
 })
 
+// RENVOIE GIF 
+
+
+bot.on('messageCreate', (message) => {
+
+  // variables contenant ma liste de mot à utiliser pour trigger un meme
+
+  let salutations = ["hi", "hey", "hello", "heya", "yo"]
+  let risitas = ["issou", "kekw", "kek"]
+  let daway = ["the way", "da way", "da we", "da weh", "da wae", " da wey", "de wey", "de way", "de weh"]
+
+  // Reply en foction de la liste
+
+  if (salutations.includes(message.content.toLowerCase())) {
+    message.reply("https://i.pinimg.com/originals/fd/75/14/fd75149899abe3e57e4c9d3bf4243e9c.gif")
+  }
+
+  if (risitas.includes(message.content.toLowerCase())) {
+    message.reply("https://thumbs.gfycat.com/DescriptiveSlushyGrizzlybear-max-1mb.gif")
+  }
+
+  if (daway.includes(message.content.toLowerCase())) {
+    message.reply("https://c.tenor.com/D_Zn-MlO4mEAAAAC/do-you-know-the-way-ugandan-knuckles.gif")
+  }
+
+})
+
 
 
 
